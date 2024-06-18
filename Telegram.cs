3dacb0c45aa14094090af9d12ebfac138df49863
Was404 <vladimirovichdanil@bk.ru>
2024-06-18 13:33:29 +0300

    test

---
+++ Telegram.cs
@@ -0,0 +1,15 @@
+namespace BrunoAssistant
+{
+    internal static class Telegram
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            
+        }
+    }
+}
